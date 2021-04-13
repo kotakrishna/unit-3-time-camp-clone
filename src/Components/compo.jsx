18279bcb447367d5ce50a31294eeaf0d79bb8bd2
k.saikrishna <k.saikrishna78@gmail.com>
2021-04-13 16:04:21 +0530

    the header footer along with routes,login,register

--- src/Components/compo.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function compo() {
-  return <div></div>;
-}
--- src/Pages/AuthRegister.jsx
+++ src/Pages/AuthRegister.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AuthRegister() {
-  return <div></div>;
+  return <div>Registration Page</div>;
 }
