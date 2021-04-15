a9eaeb513cff1566f0ec1f312081d728ebb9a14b
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2021-04-15 19:52:25 +0530

    chatbot and landingpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Routes from "./Routes/Routes";
+import ChatBot from "./Components/Chatbot"
 function App() {
   return (
     <div className="App">
       
       <Routes />
+      <Chatbot/>
     </div>
   );
 }
