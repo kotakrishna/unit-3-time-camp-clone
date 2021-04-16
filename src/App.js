84e5b970997351f9ae1e7a9fb5841698f37af83c
ajcodeskills <martinaj9514@gmail.com>
2021-04-16 15:27:02 +0530

    Logged user tasks update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       
       <Routes/>
-      <Chatbot/>
+      {/* <Chatbot/> */}
     </div>
   );
 }
--- src/Components/Chatbot.jsx
+++ src/Components/Chatbot.jsx
@@ -1,64 +1,64 @@
-import React from 'react';
-import ChatBot from 'react-simple-chatbot';
-function Chatbot (){
+// import React from 'react';
+// import ChatBot from 'react-simple-chatbot';
+// function Chatbot (){
  
-    return(
-      <ChatBot
-        steps={[
-          {
-            id: '1',
-            message: 'tell me your name',
-            trigger: '2',
-          },
-          {
-            id: '2',
-            user: true,
-            trigger: '3',
-          },
-          {
-            id: '3',
-            message: 'hii {previousValue}, how may i help you please? ',
-            trigger: '4',
-          },
+//     return(
+//       <ChatBot
+//         steps={[
+//           {
+//             id: '1',
+//             message: 'tell me your name',
+//             trigger: '2',
+//           },
+//           {
+//             id: '2',
+//             user: true,
+//             trigger: '3',
+//           },
+//           {
+//             id: '3',
+//             message: 'hii {previousValue}, how may i help you please? ',
+//             trigger: '4',
+//           },
 
 
 
-          {
-            id: '4',
-            user: true,
-            trigger: '5',
-          },
-          {
-            id: '5',
-            message: 'sure i will help you tell me more.',
-            trigger: '6',
-          },
-          {
-            id: '6',
-            user: true,
-            trigger: '7',
-          },
-          {
-            id: '7',
-            message: 'please write your query',
-            trigger: '8',
+//           {
+//             id: '4',
+//             user: true,
+//             trigger: '5',
+//           },
+//           {
+//             id: '5',
+//             message: 'sure i will help you tell me more.',
+//             trigger: '6',
+//           },
+//           {
+//             id: '6',
+//             user: true,
+//             trigger: '7',
+//           },
+//           {
+//             id: '7',
+//             message: 'please write your query',
+//             trigger: '8',
             
-          },
-          {
-            id: '8',
-            user: true,
-            trigger: '9',
-          },
-          {
-            id: '9',
-            message: 'I raised your query,our team will contact you soon.',
-            end: true,
-          },
+//           },
+//           {
+//             id: '8',
+//             user: true,
+//             trigger: '9',
+//           },
+//           {
+//             id: '9',
+//             message: 'I raised your query,our team will contact you soon.',
+//             end: true,
+//           },
 
-        ]}
-      />
-    )
-  }
+//         ]}
+//       />
+//     )
+//   }
 
-export { Chatbot};
+// export { Chatbot};
 
